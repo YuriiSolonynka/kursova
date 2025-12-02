cfddcd7698a889025c12083e32c932748b095eaa
YuriiSolonynka <yura20xa@gmail.com>
2025-12-02 17:41:42 +0200

    minor changes 2

--- backend/Models/DTOs/HallDto.cs
+++
@@ -1,9 +0,0 @@
-namespace backend.Models.DTOs
-{
-    public class HallDto
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public int Capacity { get; set; }
-    }
-}
--- backend/Services/IEmailService.cs
+++
@@ -1,7 +0,0 @@
-namespace backend.Services
-{
-    public interface IEmailService
-    {
-        Task SendEmailAsync(string toEmail, string subject, string body);
-    }
-}
